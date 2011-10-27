3e260d404f2496739856bc594f76331bcb286e27
David Gramlich <dgramlich22@gmail.com>
2011-10-27 09:59:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ehsweb::Application.config.secret_token = '655eed0f2db0ce11cef8255f0accdb2a4af4c23af2fc0a07a8147eec8cc5ee31421c64e571be6e2479d982cb279c13ce55318b8d0daf059ae71b55513e04f9a9'
